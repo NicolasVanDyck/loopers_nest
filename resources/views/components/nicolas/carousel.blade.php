d865d9464f96966097e085e623dde1a2178fd913
NicolasVanDyck <r0878921@student.thomasmore.be>
2023-01-01 14:53:30 +0100

    carousel component

---
+++ resources/views/components/nicolas/carousel.blade.php
@@ -0,0 +1,5 @@
+<div id="carouselExampleSlidesOnly" class="carousel slide relative" data-bs-ride="carousel">
+    <div class="carousel-inner relative w-full overflow-hidden">
+        {{ $slot }}
+    </div>
+</div>
